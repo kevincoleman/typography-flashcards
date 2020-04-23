85d27d0b815db40cbcad59056d761e58baf0d59b
kevincoleman <kevin@kevincoleman.io>
2020-04-23 10:11:05 -0700

    Finesse styling for mobile

--- src/components/flashcard/Flashcard.tsx
+++ src/components/flashcard/Flashcard.tsx
@@ -5,16 +5,40 @@ interface IFlashcardProps {
   typeface: any;
 }
 
-export default class Flashcard extends Component<IFlashcardProps> {
+interface IFlashcardState {
+  showAnswer: boolean;
+}
+
+export default class Flashcard extends Component<IFlashcardProps, IFlashcardState> {
+  
+  constructor(props: IFlashcardProps) {
+    super(props);
+
+    this.state = {
+      showAnswer: false
+    }
+
+    this.revealAnswer = this.revealAnswer.bind(this);
+    this.render = this.render.bind(this);
+  }
+
+  revealAnswer() {
+    this.setState({showAnswer: true});
+  }
+  
   render() {
     const { typeface } = this.props;
+    const { showAnswer } = this.state;
 
     return (
       <div className="FlashCard">
         <img src={typeface.image} className="typeface" alt={typeface.name} />
+        <div className={"answer"} onClick={this.revealAnswer}>
+          <span className={showAnswer ? "visible" : ""}>{typeface.name}</span>
+        </div>
         <div className="action-area">
-          <a href={typeface.url} target="_blank" rel="noopener noreferrer" className="action-area_see-more">See more glyphs</a>
-          <a href="/" className="action-area_get-another-one">Get another one</a>
+          <a href={typeface.url} target="_blank" rel="noopener noreferrer" className="action-area_aabbcc">Aa&nbsp;Bb&nbsp;..</a>
+          <a href="/" className="action-area_next">Next</a>
         </div>
       </div>
     );
