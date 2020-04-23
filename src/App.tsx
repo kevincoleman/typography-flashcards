5349452f92762dbcea458ad8d0709c06d4fd437b
kevincoleman <kevin@kevincoleman.io>
2020-04-23 11:11:20 -0700

    Restructure component functions & props, update alignment

--- src/App.tsx
+++ src/App.tsx
@@ -2,22 +2,12 @@ import React, { Component } from 'react';
 import Flashcard from  './components/flashcard/Flashcard';
 import './App.css';
 
-import flashcards from "./data/flashcards";
-
-
-
 export default class App extends Component {
-  
-  getRandomTypeface(): any {
-    return flashcards[Math.floor(Math.random() * flashcards.length)];
-  }
 
   render() {
     return (
       <div className="App">
-        <Flashcard
-          typeface={this.getRandomTypeface()}
-        />
+        <Flashcard />
       </div>
     );
   }
--- src/components/flashcard/Flashcard.tsx
+++ src/components/flashcard/Flashcard.tsx
@@ -1,44 +1,61 @@
 import React, { Component } from 'react';
 import './Flashcard.css';
+import flashcards from "./flashcards";
 
-interface IFlashcardProps {
-  typeface: any;
-}
+interface IFlashcardProps { }
 
 interface IFlashcardState {
+  typeface: ITypeface;
   showAnswer: boolean;
+}
+
+interface ITypeface {
+  name: string,
+  image: string,
+  url: string
 }
 
 export default class Flashcard extends Component<IFlashcardProps, IFlashcardState> {
   
-  constructor(props: IFlashcardProps) {
-    super(props);
-
-    this.state = {
-      showAnswer: false
-    }
-
-    this.revealAnswer = this.revealAnswer.bind(this);
-    this.render = this.render.bind(this);
+  state = {
+    typeface: {
+      name: "",
+      image: "",
+      url: ""
+    },
+    showAnswer: false
   }
 
-  revealAnswer() {
+  componentDidMount() {
+    this.setState({typeface: this.getNewTypeface()});
+  }
+
+  getNewTypeface = () => {
+    return flashcards[Math.floor(Math.random() * flashcards.length)];
+  }
+
+  onNextTypeface = () => {
+    this.setState({typeface: this.getNewTypeface()});
+  }
+
+  onShowAnswer = () => {
     this.setState({showAnswer: true});
   }
-  
+
   render() {
-    const { typeface } = this.props;
-    const { showAnswer } = this.state;
+    const { typeface, showAnswer } = this.state;
 
     return (
       <div className="FlashCard">
-        <img src={typeface.image} className="typeface" alt={typeface.name} />
-        <div className={"answer"} onClick={this.revealAnswer}>
+        <div className="typeface">
+          <img src={typeface.image} className="typeface_image" alt={typeface.name} />
+        </div>
+        <div className={"answer"} onClick={this.onShowAnswer}>
           <span className={showAnswer ? "visible" : ""}>{typeface.name}</span>
         </div>
         <div className="action-area">
-          <a href={typeface.url} target="_blank" rel="noopener noreferrer" className="action-area_aabbcc">Aa&nbsp;Bb&nbsp;..</a>
-          <a href="/" className="action-area_next">Next</a>
+          <a href={typeface.url} target="_blank" rel="noopener noreferrer" className="action-area_aabb">Aa&nbsp;Bb&nbsp;..</a>
+          <button onClick={this.onNextTypeface} className="action-area_next">Next</button>
         </div>
       </div>
     );
